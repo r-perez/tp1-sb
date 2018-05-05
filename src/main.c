e62b5e8312ca8de0b88842f177f771fdcf59cd6b
matheushsouza <matheus.hs97@gmail.com>
2018-05-04 21:39:17 -0300

    ATM this is basically a "test stuff" file

--- src/main.c
+++ src/main.c
@@ -1,12 +1,28 @@
 #include <stdio.h>
 
-#include "hash.h"
-#include "instructions.h"
+/* #include "hash.h" */
+/* #include "translator.h" */
+
+void constantTo9bitString (int n, char* out) {
+    int j = 0;
+    for (int i = 9; i >= 0; i--) {
+        int k = n >> i;
+        if (k & 1)
+            out[j] = '1';
+        else
+            out[j] = '0';
+        j++;
+    }
+
+    out[9] = '\0';
+}
 
 int main (void) {
+    char test[9];
 
-    for (int i = 0; i < 23; i++)
-        printf("%s %s\n", instructions[i].instruction, instructions[i].code);
+    //translateLine("stop", test);
+    constantTo9bitString(20, test);
+    printf("%s\n", test);
    
     return 0;
 }
