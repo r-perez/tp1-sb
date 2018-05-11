8217b6b6bbf17663e10d9445d35ce44e8d241f42
matheushsouza <matheus.hs97@gmail.com>
2018-05-11 15:42:42 -0300

    Revised code, errors treated, input specified, outputting proper mif format etc.

--- src/compileTest.sh
+++ src/compileTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -o prog -g -Wall file.c  file.h  main.c  symbols.c  symbols.h  translator.c  translator.h  utility.c  utility.h;
-valgrind --leak-check=full ./prog;
+gcc -o prog -g -Wall File.c  File.h  Main.c  TableOfSymbols.c  TableOfSymbols.h  Translator.c  Translator.h  Converter.c  Converter.h;
+valgrind --leak-check=full ./prog test.a --keepBinary;
